437c0a24cb3d3af82934c018e9daec0197f6793a
Ram <94901052+rr-tri@users.noreply.github.com>
2024-07-30 13:21:44 +0545

    modified:   src/vite-env.d.ts 	modified:   tsconfig.json

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,10 +3,11 @@
 
 
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+  readonly VITE_APP_TITLE: string
+  readonly VITE_CLERK_PUBLISHABLE_KEY: string;
+  readonly NODE_ENV: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
